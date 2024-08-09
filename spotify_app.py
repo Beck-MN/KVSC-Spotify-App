ed76266692b52ed5cc8e6e327bf50cab79f5fe87
Beck-MN <Beck.christensen@gmail.com>
2024-08-08 22:06:11 -0500

    added if statement for search result and tested streamlit

--- spotify_app.py
+++ spotify_app.py
@@ -28,25 +28,22 @@ today = str(date.today())
 # Connect to spotify an give proper permissions for playlist customization
 sp = spotipy.Spotify(
     auth_manager=SpotifyOAuth(
-        client_id=SpotifyOAuth.client_id,
-        client_secret=SpotifyOAuth.client_secret,
-        redirect_uri=SpotifyOAuth.redirect_uri,
+        client_id="7444209f2bc34f7ba86b8ce6d7fe5ab7",
+        client_secret="c2b5526ba53c46cdb770351224ce9612",
+        redirect_uri="http://localhost:8080",
         scope="user-top-read,user-library-read,user-read-recently-played,playlist-modify-private,playlist-modify-public",
     )
 )
 
 # Organize NACC chart into needed columns and group by category
 keep = ["TW", "Artist", "Recording", "Label"]
-file = st.file_uploader(
-    "Upload a CSV or Excel file of the most recent NACC top 30 albums", type=["csv"]
-)
+file = st.file_uploader("Upload a CSV or Excel file of the most recent NACC top 30 albums", type=["csv"])
 
+# Make dataframe of file
+nacc_df = pd.read_csv(file, usecols=keep)
+st.dataframe(nacc_df)
 
-if file is not None:
-
-    # Make dataframe of file
-    nacc_df = pd.read_csv(file, usecols=keep)
-    st.dataframe(nacc_df)
+if file is not None and st.button("Generate Playlist"):
 
     # Establish main user (only me for now)
     user_id = sp.me()["id"]
@@ -64,25 +61,34 @@ if file is not None:
 
         # Get album name
         search = row["Recording"]
+        artist = row["Artist"]
 
         # search spotify for album
-        result = sp.search(q="album:" + search, limit=1, type="album", market="US")
+        query = f'album:{search} artist:{artist}'
+        result = sp.search(q=query, limit=1, type="album", market="US")
         search_result = result["albums"]["items"]
-        st.write(
-            search_result[0]["name"],
-            "by",
-            search_result[0]["artists"][0]["name"],
-            "added, Spotify ID:",
-            search_result[0]["id"],
-        )
 
-        # Organize search results for playlist
-        tracks = sp.album_tracks(search_result[0]["id"])
-        songs = tracks["items"]
+        # Check if spotify was able to find the album
+        if search_result:   
+            st.write(
+                search_result[0]["name"],
+                "by",
+                search_result[0]["artists"][0]["name"],
+                "added, Spotify ID:",
+                search_result[0]["id"],
+            )
 
-        # Add songs to playlist
-        for track in songs:
-            sp.playlist_add_items(playlist_id=playlist["id"], items=[track["id"]])
+            # Organize search results for playlist
+            tracks = sp.album_tracks(search_result[0]["id"])
+            songs = tracks["items"]
+
+            # Add songs to playlist
+            for track in songs:
+                sp.playlist_add_items(playlist_id=playlist["id"], items=[track["id"]])
+        
+        # Spotify couln't find the album
+        else:
+            st.write("Couldn't find",search,"by",artist)
 
     st.write(
         "Playlist Generated! Find it in your library or at: ",
@@ -99,7 +105,7 @@ if file is not None:
 
     # Generate data frame of audio features
     audio_features_df = pd.DataFrame(audio_features)
-    matrix_df = audio_features_df[
+    audio_features_df = audio_features_df[
         [
             "danceability",
             "energy",
@@ -113,7 +119,9 @@ if file is not None:
     ]
 
     # Create correlation matrix of audio features
-    matrix = matrix_df.corr()
+    matrix = audio_features_df.corr()
 
     # Display matrix
     sns.heatmap(matrix, annot=True, cmap="coolwarm")
+
+    st.pyplot(plt.gcf())
