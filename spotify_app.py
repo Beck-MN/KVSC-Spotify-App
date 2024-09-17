411832fc2017e9afb487e19429e935af4660d65f
Beck Christensen <Beck.christensen@gmail.com>
2024-09-17 13:00:25 -0500

    Web Scraper

--- spotify_app.py
+++ spotify_app.py
@@ -23,6 +23,10 @@ import matplotlib.pyplot as plt
 # Date and time for labeling
 from datetime import date
 
+# Webscraper libraries
+import requests
+from bs4 import BeautifulSoup
+
 today = str(date.today())
 
 # Connect to spotify an give proper permissions for playlist customization
@@ -35,16 +39,61 @@ sp = spotipy.Spotify(
     )
 )
 
+# Funtion to get a return the current KVSC top 30 via a web scraper
+def get_nacc_chart():
+
+    request_headers = {
+        "referer": "https://www.kvsc.org/wp-content/themes/kvsc/css/style.css?ver=6.6.1",
+        "accept-language": "en-US,en;q=0.9",
+        "sec-ch-ua": '"Not/A)Brand";v="8", "Chromium";v="126", "Opera GX";v="112"',
+        "sec-ch-ua-platform": "Windows",
+        "sec-ch-ua-platform-version": '"10.0.0"',
+        "sec-ch-viewport-width": "792",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+    }
+
+    response = requests.get(
+        "https://www.kvsc.org/music/nacc-charts/", headers=request_headers
+    )
+
+    soup = BeautifulSoup(response.text, "html.parser")
+    album_list = soup.find("body")
+    album_list_items = album_list.find("ol", class_="chart")
+    positions = []
+    artists = []
+    albums = []
+    labels = []
+
+    chart_items = album_list_items.find_all("li")
+
+    for item in chart_items:
+
+        if item.find("span", class_="position"):
+            position = item.find("span", class_="position").text.strip()
+            artist = item.find("span", class_="artist").text.strip()
+            album = item.find("span", class_="album").text.strip()
+            label = item.find("span", class_="label").text.strip()
+
+            positions.append(position)
+            artists.append(artist)
+            albums.append(album)
+            labels.append(label)
+
+    df = pd.DataFrame(
+        {"Position": positions, "Artist": artists, "Recording": albums, "Label": labels}
+    )
+
+    return df
+
 # Organize NACC chart into needed columns and group by category
 keep = ["TW", "Artist", "Recording", "Label"]
-file = st.file_uploader("Upload a CSV or Excel file of the most recent NACC top 30 albums", type=["csv"])
 
-if file is not None and st.button("Generate Playlist"):
+if st.button("Generate Playlist"):
 
     # Make dataframe of file
-    nacc_df = pd.read_csv(file, usecols=keep)
+    nacc_df = get_nacc_chart()
     st.dataframe(nacc_df)
-    
+
     # Establish main user (only me for now)
     user_id = sp.me()["id"]
 
@@ -69,7 +118,7 @@ if file is not None and st.button("Generate Playlist"):
         search_result = result["albums"]["items"]
 
         # Check if spotify was able to find the album
-        if search_result:   
+        if search_result:
             st.write(
                 search_result[0]["name"],
                 "by",
@@ -85,47 +134,47 @@ if file is not None and st.button("Generate Playlist"):
             # Add songs to playlist
             for track in songs:
                 sp.playlist_add_items(playlist_id=playlist["id"], items=[track["id"]])
-        
+
         # Spotify couln't find the album
         else:
-            st.write("Couldn't find",search,"by",artist)
-
+            st.write("Couldn't find", search, "by", artist)
 
     st.write(
         "Playlist Generated! Find it in your library or at: ",
-        playlist["external_urls"]["spotify"],)
-    
+        playlist["external_urls"]["spotify"],
+    )
+
     st.write("Generating correlation matix of the auido features of the NACC top 30...")
 
-        # Get track ID's of songs from generated playlist
-        # Get audio features of each track
-    playlist_tracks = sp.playlist_tracks(playlist['id'])
-    tracks = playlist_tracks['items']
-    track_ids = [track['track']['id'] for track in tracks]
+    # Get track ID's of songs from generated playlist
+    # Get audio features of each track
+    playlist_tracks = sp.playlist_tracks(playlist["id"])
+    tracks = playlist_tracks["items"]
+    track_ids = [track["track"]["id"] for track in tracks]
     audio_features = sp.audio_features(track_ids)
 
-        # Generate data frame of audio features
+    # Generate data frame of audio features
     audio_features_df = pd.DataFrame(audio_features)
     audio_features_df = audio_features_df[
-            [
-                "danceability",
-                "energy",
-                "speechiness",
-                "acousticness",
-                "instrumentalness",
-                "liveness",
-                "valence",
-                "tempo",
-            ]
+        [
+            "danceability",
+            "energy",
+            "speechiness",
+            "acousticness",
+            "instrumentalness",
+            "liveness",
+            "valence",
+            "tempo",
         ]
+    ]
 
-        # Create correlation matrix of audio features
+    # Create correlation matrix of audio features
     matrix = audio_features_df.corr()
 
-        # Display matrix
+    # Display matrix
     sns.heatmap(matrix, annot=True, cmap="rocket")
 
     st.pyplot(plt.gcf())
-        
 
-   
+
+
