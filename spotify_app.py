e0a7b6e66837e01af922e84357aede6310569ff1
Beck-MN <Beck.christensen@gmail.com>
2024-09-17 16:32:29 -0500

    adding definition and QOL stuff

--- spotify_app.py
+++ spotify_app.py
@@ -39,22 +39,23 @@ sp = spotipy.Spotify(
     )
 )
 
+request_headers = {
+    "referer": "https://www.kvsc.org/wp-content/themes/kvsc/css/style.css?ver=6.6.1",
+    "accept-language": "en-US,en;q=0.9",
+    "sec-ch-ua": '"Not/A)Brand";v="8", "Chromium";v="126", "Opera GX";v="112"',
+    "sec-ch-ua-platform": "Windows",
+    "sec-ch-ua-platform-version": '"10.0.0"',
+    "sec-ch-viewport-width": "792",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+}
+
+response = requests.get(
+    "https://www.kvsc.org/music/nacc-charts/", headers=request_headers
+)
+
+
 # Funtion to get a return the current KVSC top 30 via a web scraper
 def get_nacc_chart():
-
-    request_headers = {
-        "referer": "https://www.kvsc.org/wp-content/themes/kvsc/css/style.css?ver=6.6.1",
-        "accept-language": "en-US,en;q=0.9",
-        "sec-ch-ua": '"Not/A)Brand";v="8", "Chromium";v="126", "Opera GX";v="112"',
-        "sec-ch-ua-platform": "Windows",
-        "sec-ch-ua-platform-version": '"10.0.0"',
-        "sec-ch-viewport-width": "792",
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
-    }
-
-    response = requests.get(
-        "https://www.kvsc.org/music/nacc-charts/", headers=request_headers
-    )
 
     soup = BeautifulSoup(response.text, "html.parser")
     album_list = soup.find("body")
@@ -83,18 +84,15 @@ def get_nacc_chart():
         {"Position": positions, "Artist": artists, "Recording": albums, "Label": labels}
     )
 
+    df = df.drop(index=0)
+
     return df
 
-# Organize NACC chart into needed columns and group by category
-keep = ["TW", "Artist", "Recording", "Label"]
 
-if st.button("Generate Playlist"):
+# Function to creat a spotify playlist of the top 30
+def generate_playlist(nacc_df):
 
-    # Make dataframe of file
-    nacc_df = get_nacc_chart()
-    st.dataframe(nacc_df)
-
-    # Establish main user (only me for now)
+    # Get user id
     user_id = sp.me()["id"]
 
     # Create Playlist
@@ -135,7 +133,7 @@ if st.button("Generate Playlist"):
             for track in songs:
                 sp.playlist_add_items(playlist_id=playlist["id"], items=[track["id"]])
 
-        # Spotify couln't find the album
+            # Spotify couln't find the album
         else:
             st.write("Couldn't find", search, "by", artist)
 
@@ -144,37 +142,88 @@ if st.button("Generate Playlist"):
         playlist["external_urls"]["spotify"],
     )
 
-    st.write("Generating correlation matix of the auido features of the NACC top 30...")
+    return playlist
 
-    # Get track ID's of songs from generated playlist
-    # Get audio features of each track
-    playlist_tracks = sp.playlist_tracks(playlist["id"])
-    tracks = playlist_tracks["items"]
-    track_ids = [track["track"]["id"] for track in tracks]
-    audio_features = sp.audio_features(track_ids)
 
-    # Generate data frame of audio features
-    audio_features_df = pd.DataFrame(audio_features)
-    audio_features_df = audio_features_df[
-        [
-            "danceability",
-            "energy",
-            "speechiness",
-            "acousticness",
-            "instrumentalness",
-            "liveness",
-            "valence",
-            "tempo",
+# Gets the update date for the top 30 on kvsc.org
+def get_date():
+
+    soup = BeautifulSoup(response.text, "html.parser")
+    body = soup.find("body")
+    div = body.find("div", class_="content clearfix")
+    date = div.find("h3")
+
+    return date
+
+
+# Gets the custom paragraph made each week for the top 30
+def get_paragraph():
+
+    soup = BeautifulSoup(response.text, "html.parser")
+    body = soup.find("body")
+    div = body.find("div", class_="content clearfix")
+    p1 = div.find("p")
+    p2 = p1.find_next("p")
+
+    return p2
+
+
+# Main function
+def main():
+
+    # Organize NACC chart into needed columns and group by category
+    keep = ["TW", "Artist", "Recording", "Label"]
+
+    # Get the current top 30 from kvsc.org
+    nacc_df = get_nacc_chart()
+
+    st.header("KVSC Weekly Top 30", anchor=None, divider="red")
+    st.write(
+        "The North American College and Community radio, or NACC chart, tabulates weekly the top 200 artists being spun on about 200 member stations. Member stations represent a vast array of non-commercial college and community-based radio services throughout the US and Canada. NACC also compiles charts on a number of specialty genres, including jazz, blues, folk, and hip-hop."
+    )
+    st.write(get_paragraph().text)
+    st.subheader(get_date().text, anchor=None, divider="gray")
+    st.dataframe(nacc_df, hide_index=True)
+
+    if st.button("Generate Spotify Playlist"):
+
+        #
+        playlist = generate_playlist(nacc_df)
+
+        st.write(
+            "Generating correlation matix of the auido features of the NACC top 30..."
+        )
+
+        # Get track ID's of songs from generated playlist
+        # Get audio features of each track
+        playlist_tracks = sp.playlist_tracks(playlist["id"])
+        tracks = playlist_tracks["items"]
+        track_ids = [track["track"]["id"] for track in tracks]
+        audio_features = sp.audio_features(track_ids)
+
+        # Generate data frame of audio features
+        audio_features_df = pd.DataFrame(audio_features)
+        audio_features_df = audio_features_df[
+            [
+                "danceability",
+                "energy",
+                "speechiness",
+                "acousticness",
+                "instrumentalness",
+                "liveness",
+                "valence",
+                "tempo",
+            ]
         ]
-    ]
 
-    # Create correlation matrix of audio features
-    matrix = audio_features_df.corr()
+        # Create correlation matrix of audio features
+        matrix = audio_features_df.corr()
 
-    # Display matrix
-    sns.heatmap(matrix, annot=True, cmap="rocket")
+        # Display matrix
+        sns.heatmap(matrix, annot=True, cmap="rocket")
 
-    st.pyplot(plt.gcf())
-
+        st.pyplot(plt.gcf())
 
 
+if __name__ == "__main__":
+    main()
